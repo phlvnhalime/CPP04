eda5f1d0aba80ae16b6a523bb81ae90b03450fee
Halime Pehlivan <halimephlvn@icloud.com>
2025-06-25 16:01:30 +0200

    I will continue tonight

---
+++ ex00/Animal.cpp
@@ -0,0 +1,30 @@
+
+#include "Animal.hpp"
+
+Animal::Animal(): name("Animal"){
+    std::cout << GREEN <<"Animal default constructor called" << RESET << std::endl;
+}
+
+Animal::Animal(const std::string& copy): name(copy.name){
+    std::cout << "Animal copy Constructor called" << std::endl;
+}
+
+Animal& Animal::operator=(const Animal& copy){
+    if(this != &copy)
+    {
+        name = copy.name;
+    }
+    std::cout << "Animal assignment opperator called" << std::endl;
+    return *this;
+}
+
+Animal::~Animal(){
+    std::cout << "Animal destructor called" << std::endl;
+}
+void Animal::makeSound() const{
+    std::cout << "Animal sounds" << std::endl;
+}
+
+std::string Animal::getAnimal() const{
+    return name;
+}
